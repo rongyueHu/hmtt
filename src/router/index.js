40ade7fdfef6e854573b30442c624b04e928da40
rongyueHu <1752333046@qq.com>
2022-06-26 11:44:34 +0800

    路由配置完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Layout from '@/views/Layout'// 父页面不用懒加载
+const Home = () => import('@/views/Home')
+const My = () => import('@/views/My')
+const Question = () => import('@/views/Question')
+const Video = () => import('@/views/Video')
+const Login = () => import('@/views/Login')
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      { path: 'home', component: Home },
+      { path: 'video', component: Video },
+      { path: 'question', component: Question },
+      { path: 'my', component: My }
+    ]
+  },
+  {
+    path: '/login', component: Login
+  }
 ]
 
 const router = new VueRouter({
